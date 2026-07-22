27539d315b8dc1b6aa0fef89ad084cfa0586cf61
IT25104034 <it25104034@my.sliit.lk>
2026-07-22 12:17:01 +0530

    q2 incomplete

---
+++ MarksList.java
@@ -0,0 +1,9 @@
+import util.java.Scanner;
+
+    public class MarksList{
+
+    public static void main(String [] args){
+
+ 	Scanner input = new Scanner(System.in);
+
+ System.out.print("Enter the  
