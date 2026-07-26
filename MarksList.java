9a22e4211123c71928a9c29bb5c12960cc6a69f2
it25104034 <it25104034@my.sliit.lk>
2026-07-26 10:51:42 +0530

    Menu listing

--- MarksList.java
+++ MarksList.java
@@ -10,3 +10,13 @@ import util.java.Scanner;
         int n = sc.nextInt();
         int marks[][] = new int[n][3];
 
+         while (true) {
+            System.out.println("\n1. Add marks");
+            System.out.println("2. Update marks");
+            System.out.println("3. Average of subject");
+            System.out.println("4. Average of student");
+            System.out.println("5. Total of student");
+            System.out.println("6. Exit");
+            System.out.print("Enter choice: ");
+            int ch = sc.nextInt();
+
